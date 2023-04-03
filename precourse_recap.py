d7a569ab73603b8fa8f8160bb8119cf8b1702fd9
C <calums88@gmail.com>
2023-04-03 21:04:14 +0100

    first commit

---
+++ precourse_recap.py
@@ -0,0 +1,9 @@
+name = "Calum"
+age = 34
+months_in_year = 12
+
+def total_months_alive():
+	age_months = age * months_in_year
+	print(f"Calum has been alive for {age_months} months!")
+    
+total_months_alive()
